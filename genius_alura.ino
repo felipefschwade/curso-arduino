f807207255000cf2345da0b45d63bc5556a98d7c
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-03-22 22:44:47 -0400

    Aula 6 - Realizando leituras analógicas para gerar números aleatórios

--- genius_alura.ino
+++ genius_alura.ino
@@ -7,28 +7,33 @@
 #define BOTAO_AMARELO 9
 #define BOTAO_AZUL 10
 #define BOTAO_VERMELHO 11
+#define INDEFINIDO -1
 
 int sequenciaLuzes[TAMANHO_SEQUENCIA]; //Define uma variavel sequencia em escopo global
 
 void setup() {
    iniciaPortas();
-   iniciaJogo();
    Serial.begin(9600);
+   iniciaJogo();
 }
 
 void loop() {
-    int estado = digitalRead(BOTAO_VERDE);
-    Serial.println(estado);
+    piscaSequencia2();
+    delay(500);
+    for (int i = 0; i < TAMANHO_SEQUENCIA; i++) {
+      piscaLed(sequenciaLuzes[i]);
+    }
 }
 
 //Função Para piscar os leds
 //@param = Número da porta digital do Led
-void piscaLed(int portaLed){
+int piscaLed(int portaLed){
     digitalWrite(portaLed, HIGH); //Altera a porta para ligada
     delay(1000); //Espera 1 Sec
     digitalWrite(portaLed, LOW); //Altera a porta para desligada
     delay(500); //Espera 0,5 sec
     Serial.println(portaLed); //Imprime a porta
+    return portaLed;
   }
   
 //Define e Inicia todas as portas que serão utilizadas
@@ -44,10 +49,11 @@ void iniciaPortas() {
   }
 //Define uma sequencia que os leds piscaram no começo do jogo
 void iniciaJogo() {
-    sequenciaLuzes[0] = LED_AZUL;
-    sequenciaLuzes[1] = LED_AMARELO;
-    sequenciaLuzes[2] = LED_VERMELHO;
-    sequenciaLuzes[3] = LED_VERDE;
+    int seed = analogRead(0);
+    randomSeed(seed);
+    for(int i = 0; i < TAMANHO_SEQUENCIA; i++) {
+        sequenciaLuzes[i] = sorteiaCor();
+      } 
   }
 //Define uma sequencia onde todos os leds piscam Simultaneamente
 void piscaSequencia2() { 
@@ -62,5 +68,25 @@ void piscaSequencia2() {
     digitalWrite(LED_AMARELO, LOW);
     digitalWrite(LED_VERMELHO, LOW);
     digitalWrite(LED_VERDE, LOW);
+  }
+//Define uma função que retorna o LED cujo botão foi pressionado
+int checaRespostaJogador() {
+    if (digitalRead(BOTAO_VERDE) == LOW) {
+        piscaLed(LED_VERDE);
+      }
+     if (digitalRead(BOTAO_AZUL) == LOW) {
+        piscaLed(LED_AZUL);
+      }
+     if (digitalRead(BOTAO_VERMELHO) == LOW) {
+        piscaLed(LED_VERMELHO);
+      }
+     if (digitalRead(BOTAO_AMARELO) == LOW) {
+        piscaLed(LED_AMARELO);
+      }
+     return INDEFINIDO;
+  }
+//Define uma função que sorteia uma cor aleatória
+int sorteiaCor() {
+    return random(LED_VERDE, LED_VERMELHO + 1);
   }
   
